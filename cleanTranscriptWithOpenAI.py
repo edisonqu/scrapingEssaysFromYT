26bd273c2cc8cc16a0b004431017d0cccfc22d30
edison <edisonquu@gmail.com>
2022-10-24 23:02:23 -0400

    added in the openai essay

--- cleanTranscriptWithOpenAI.py
+++ cleanTranscriptWithOpenAI.py
@@ -1,3 +1,25 @@
 
+import os
+import openai
+from dotenv import load_dotenv
 
-# def cleanTranscripts():
+load_dotenv()
+
+openai.api_key = os.getenv("API")
+
+
+# prompt is preset and pre-determined
+prompt = input("Enter your prompt: ")
+
+response = openai.Completion.create(
+  model="text-davinci-002",
+  prompt= prompt,
+  temperature=0.7,
+  max_tokens=2599,
+  top_p=1,
+  frequency_penalty=0,
+  presence_penalty=0
+)
+
+print(response)
+print(response["choices"][0]["text"])
